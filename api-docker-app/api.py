33af9e6f5a082a42b5aab631613be3c4058e9e0c
MeghanFair <mkfair05@gmail.com>
2019-07-23 14:34:58 -0700

    Able to get the feature layer data and table and add it to a webmap object. Need to now render the webmap object on the front end.

--- api-docker-app/api.py
+++ api-docker-app/api.py
@@ -1,31 +1,48 @@
 import flask
 from flask import request, jsonify
 from arcgis.gis import GIS   #python wrapper
+from arcgis.mapping import WebMap
+# import json
 from flask_cors import CORS
 
 app = flask.Flask(__name__)
 CORS(app)
 
 gis = GIS("https://arcgis.com", "mkfair05", "2E5nSda4nZUKbCp")
+wm = WebMap()
+
 
 @app.route('/layer', methods=['GET'])
-def getLayer():
+def getLayerData():
     search_result = gis.content.search(query="", item_type="Feature *")
     first_item = search_result[0]
     item_id = first_item.id
-
     item = gis.content.get(item_id)
-    return item_id
+    # return first_item
 
-    
-@app.route('/user', methods=['GET'])
+    layer = first_item.layers
+    lay = layer[0]
+
+    wm.add_layer(lay)
+
+    web_map_properties = {
+        'title':'BC Lightstation Sea Surface Data',
+        'snippet':'This map shows the temperature and salinity of the sea surface at high tide'+\
+            'for lightstations on the BC coast.',
+        'tags':'ArcGIS Python API'
+    }
+    webmap_item = wm.save(item_properties=web_map_properties)
+    webmap_obj = WebMap(webmap_item)
+    return webmap_obj
+
+
+# @app.route('/user', methods=['GET'])
 def login():
     # connect to instance of ArcGIS Online
     return gis.properties.user.username
 
 
-
-@app.route('/hello')
+# @app.route('/hello')
 def helloworld():
     return "hello world"
 
@@ -33,8 +50,8 @@ def helloworld():
 if __name__ == '__main__':
     app.run(debug=True)
     # login()
-    # getLayer()
-
+    # getLayerData()
+    # getMap()
 
 # @app.route('/', methods=['GET'])
 
