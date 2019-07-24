bfe5ea7e426d1e6dee57166bd924ca0f751300e6
MeghanFair <mkfair05@gmail.com>
2019-07-24 09:33:43 -0700

    Cleaned up API.Added dropdown for lighthouses. Sort of got the webmap working, need to figure out async request.

--- api-docker-app/api.py
+++ api-docker-app/api.py
@@ -1,63 +1,24 @@
 import flask
 from flask import request, jsonify
-from arcgis.gis import GIS   #python wrapper
-from arcgis.mapping import WebMap
-# import json
 from flask_cors import CORS
+from arcgis.gis import GIS
+from arcgis.mapping import WebMap
 
 app = flask.Flask(__name__)
+
+# FUCK YOU CORS YOU ARE THE WORST
 CORS(app)
 
 gis = GIS("https://arcgis.com", "mkfair05", "2E5nSda4nZUKbCp")
-wm = WebMap()
 
-
-@app.route('/layer', methods=['GET'])
-def getLayerData():
-    search_result = gis.content.search(query="", item_type="Feature *")
+@app.route('/map', methods=['GET'])
+def getMap():
+    search_result = gis.content.search(query="", item_type="Web Map")
+    
     first_item = search_result[0]
     item_id = first_item.id
-    item = gis.content.get(item_id)
-    # return first_item
 
-    layer = first_item.layers
-    lay = layer[0]
-
-    wm.add_layer(lay)
-
-    web_map_properties = {
-        'title':'BC Lightstation Sea Surface Data',
-        'snippet':'This map shows the temperature and salinity of the sea surface at high tide'+\
-            'for lightstations on the BC coast.',
-        'tags':'ArcGIS Python API'
-    }
-    webmap_item = wm.save(item_properties=web_map_properties)
-    webmap_obj = WebMap(webmap_item)
-    return webmap_obj
-
-
-# @app.route('/user', methods=['GET'])
-def login():
-    # connect to instance of ArcGIS Online
-    return gis.properties.user.username
-
-
-# @app.route('/hello')
-def helloworld():
-    return "hello world"
-
+    return item_id
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # login()
-    # getLayerData()
-    # getMap()
-
-# @app.route('/', methods=['GET'])
-
-# # A route to return all of the available entries in our catalog.
-# @app.route('/api/', methods=['GET'])
-# def api_all():
-#     return jsonify(books)
-
-# app.run()
--- react-front-end/src/MainPage.js
+++ react-front-end/src/MainPage.js
@@ -7,20 +7,17 @@ export default class MainPage extends React.Component {
     constructor(props){
         super(props)
         this.state={
-            item_id:""
+            webserver_ip: 'http://localhost:5000',
+            item_id: ""
         }
-        
     }
-    
+
     componentDidMount() {
-        // axios.get('http://localhost:5000/hello')
-        // .then(response => {
-        //     console.log(response.data)
-        //     this.setState({str: response.data})
-        // })
-            
-        // .catch(err => console.log('Error: ' + err))
-        axios.get('http://localhost:5000/layer')
+        this.getMap()
+    }
+
+    getMap = async() => {
+        await axios.get(this.state.webserver_ip + '/map')
         .then(response => {
             console.log(response.data)
             this.setState({item_id: response.data})
@@ -28,15 +25,7 @@ export default class MainPage extends React.Component {
         .catch(err => console.log('Error: ' + err))
     }
 
-    getData = () => {
-        axios.get('http://localhost:5000/layer')
-        .then(response => {
-            console.log(response.data)
-            this.setState({item_id: response.data})
-        })
-        .catch(err => console.log('Error: ' + err))
-    }
-
+    // TODO: Get lighthouse data from backend
     render() {
         return (
             <div className="App">
@@ -48,11 +37,31 @@ export default class MainPage extends React.Component {
                         <p>
                         Meghan Fair
                         </p>
-                        <button className="button" onClick={this.getData}>{this.state.str}</button>
                     </div>
-                    <div style={{ width: '100vw', height: '100vh' }}>
-                        <WebMap id={this.state.item_id} />
+                    <select>
+                        <option href="#/action-1">Amphitrite Point</option>
+                        <option href="#/action-2">Bonilla Island</option>
+                        <option href="#/action-3">Chrome Island</option>
+                        <option href="#/action-3">Departure Bay</option>
+                        <option href="#/action-3">Egg Island</option>
+                        <option href="#/action-3">Entrance Island</option>
+                        <option href="#/action-3">Kains Island</option>
+                        <option href="#/action-3">Langara Island</option>
+                        <option href="#/action-3">McInnes Island</option>
+                        <option href="#/action-3">Nootka Point</option>
+                        <option href="#/action-3">Pine Island</option>
+                        <option href="#/action-3">Race Rocks</option>
+                    </select>
+
+                    <div className='Blank-space'></div>
+
+                    <button onClick={this.getMap}>Get Map</button>
+                    
+            {/* // TODO: Render lighthouse feature layer */}
+                    <div style={{ width: '75vw', height: '75vh' }}>
+                        <WebMap id={this.state.item_id} />    
                     </div>
+
                 </header>
             </div>
         )
